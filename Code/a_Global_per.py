886c15c8540b8ae9b2983a54c703b12f34bb0e87
SemenikhinaVioletta <violettas2005@gmail.com>
2024-12-10 14:52:16 +0300

    redisain Global_Per

--- Code/a_Global_per.py
+++ Code/a_Global_per.py
@@ -7,6 +7,17 @@ contract_statuses = []
 
 
 def create_combobox(current_status, row_index, parent_frame):
+    """
+    Creates a Combobox widget with available client IDs based on the current status.
+
+    Parameters:
+    current_status (str): The current status of the client.
+    row_index (int): The row index where the Combobox should be placed in the parent frame.
+    parent_frame (Tkinter.Frame): The parent frame where the Combobox should be placed.
+
+    Returns:
+    Tkinter.Combobox: The created Combobox widget.
+    """
     available_methods = []
     for client in client_statuses:
         if client.get_status() != current_status:
